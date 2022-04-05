f9e0df53e900bf6814c06b5af0dd599d227f5ca0
Shihao-Zhong <szhong59@massmutual.com>
2022-04-04 20:45:53 -0400

    apply vaif

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 mvn clean install
-cp opentelemetry-javaagent-all.jar opentelemetry-app-a/ 
+cp opentelemetry-javaagent-all.jar opentelemetry-app-a/
 cp opentelemetry-javaagent-all.jar opentelemetry-app-b/
+cp enable.json opentelemetry-app-a/
+cp enable.json opentelemetry-app-b/
 docker-compose up --build
