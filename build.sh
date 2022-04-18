5b0e92adf52133505740addd2e11eb0316e4a341
Shihao-Zhong <szhong59@massmutual.com>
2022-04-18 11:53:30 -0400

    update class

--- build.sh
+++ build.sh
@@ -3,4 +3,6 @@ cp opentelemetry-javaagent-all.jar opentelemetry-app-a/
 cp opentelemetry-javaagent-all.jar opentelemetry-app-b/
 cp enable.json opentelemetry-app-a/
 cp enable.json opentelemetry-app-b/
+cp vaif.json opentelemetry-app-a/
+cp vaif.json opentelemetry-app-b/
 docker-compose up --build
